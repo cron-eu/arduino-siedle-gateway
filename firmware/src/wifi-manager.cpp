1a7ab8bc0840f75a07d3395b89d781ca5eb68b94
Remus Lazar <rl@cron.eu>
2020-06-30 20:03:19 +0200

    Fix compiler errors for SAMD targets

--- firmware/src/wifi-manager.cpp
+++ firmware/src/wifi-manager.cpp
@@ -56,15 +56,26 @@ void WiFiManagerClass::connect() {
     String ssid = F(SECRET_SSID);
     String pass = F(SECRET_PASS);
     Debug.print(String(F("Connecting to WiFi Network '")) + ssid + "' .. ");
+    #ifdef ARDUINO_ARCH_SAMD
+    WiFi.begin(SECRET_SSID, SECRET_PASS);
+    #elif defined(ESP8266)
     WiFi.begin(ssid, pass); // this will run async
-}
+
+    #endif
+    }
 
 void WiFiManagerClass::printWifiStatus() {
     // print the SSID of the network you're attached to:
     Debug.println(F("done!"));
 
+    #ifdef ARDUINO_ARCH_SAMD
+    auto localIP = WiFi.localIP();
+    #elif defined(ESP8266)
+    auto localIP = WiFi.localIP().toString();
+    #endif
+
     // print ip and rssi of the current link
-    Debug.println(String(F("Local IP: ")) + WiFi.localIP().toString()
+    Debug.println(String(F("Local IP: ")) + localIP
                 + F(", signal strength (RSSI): ") + WiFi.RSSI() + F(" dBm."));
 }
 
