d3b709d838872ab4e09df2d63509f0475b662c20
Remus Lazar <rl@cron.eu>
2020-06-30 11:36:31 +0200

    Make the existing RTCSyncClass ESP8266 compatible

--- firmware/src/rtc.h
+++ firmware/src/rtc.h
@@ -6,17 +6,32 @@
 #define FIRMWARE_RTC_H
 
 #include <Arduino.h>
+
+#ifdef ARDUINO_ARCH_SAMD
 #include <WiFiNINA.h>
 #include <RTCZero.h>
+#elif defined(ESP8266)
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#endif
 
 class RTCSyncClass {
 public:
+    #ifdef ARDUINO_ARCH_SAMD
     RTCSyncClass() : rtc() { }
+    #elif defined(ESP8266)
+    RTCSyncClass() : ntp(ntpUDP) { }
+    #endif
     void begin() {
         lastMillis = 0;
         initialized = false;
         bootEpoch = 0;
+        #ifdef ARDUINO_ARCH_SAMD
         rtc.begin();
+        #elif defined(ESP8266)
+        ntp.begin();
+        #endif
     }
 
     void loop() {
@@ -28,10 +43,16 @@ public:
 
 
         if (millis() - lastMillis >= interval) {
+            #ifdef ARDUINO_ARCH_SAMD
             auto epoch = WiFi.getTime();
+            #elif defined(ESP8266)
+            auto epoch = ntp.getEpochTime();
+            #endif
             if (epoch != 0) {
                 initialized = true;
+                #ifdef ARDUINO_ARCH_SAMD
                 rtc.setEpoch(epoch);
+                #endif
                 if (bootEpoch == 0) {
                     bootEpoch = epoch;
                 }
@@ -42,11 +63,20 @@ public:
     }
     unsigned long bootEpoch;
     unsigned long getEpoch() {
+        #ifdef ARDUINO_ARCH_SAMD
         return rtc.getEpoch();
+        #elif defined(ESP8266)
+        return ntp.getEpochTime();
+        #endif
     }
 
 private:
+    #ifdef ARDUINO_ARCH_SAMD
     RTCZero rtc;
+    #elif defined(ESP8266)
+    WiFiUDP ntpUDP;
+    NTPClient ntp;
+    #endif
     unsigned long lastMillis;
     bool initialized;
 
