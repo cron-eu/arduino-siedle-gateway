5621822db859089e72804857b20d876fdb2143bb
Remus Lazar <remus@lazar.info>
2023-08-07 10:54:06 +0200

    Rework webserver logic
    Do not block the main runloop while the client is sending data

--- firmware/lib/WebServer/WebServer.cpp
+++ firmware/lib/WebServer/WebServer.cpp
@@ -16,29 +16,45 @@ void WebServer::begin() {
 
 void WebServer::loop() {
 
+    static String *currentLine;
+
+    switch (status) {
+    case WebServerStatus::web_server_idle:
+
 #ifdef ARDUINO_ARCH_SAMD
-    WiFiClient client = _server.available();   // listen for incoming clients
+        client = _server.available();   // listen for incoming clients
 #elif defined(ESP8266)
-    WiFiClient client = _server.accept();   // listen for incoming clients
+        client = _server.accept();   // listen for incoming clients
 #endif
 
-    if (client) {                             // if you get a client,
-        String currentLine = "";                // make a String to hold incoming data from the client
-        while (client.connected()) {            // loop while the client's connected
-            if (client.available()) {             // if there's bytes to read from the client,
-                char c = client.read();             // read a byte, then
-                if (c == '\n') {                    // if the byte is a newline character
+        if (client) {
+            status = WebServerStatus::web_server_connected;
+            currentLine = new String();
+        }
+        break;
 
-                    // if the current line is blank, you got two newline characters in a row.
-                    // that's the end of the client HTTP request, so send a response:
-                    if (currentLine.length() == 0) {
-                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-                        // and a content-type so the client knows what's coming, then a blank line:
-                        client.println("HTTP/1.1 200 OK");
-                        client.println("Content-type:text/html");
-                        client.println();
+    case WebServerStatus::web_server_connected:
 
-                        client.print("<h1>Arduino Doorbell</h1>");
+        if (!client.connected()) {
+            // bail out if the current connection has been closed by the client or due to network errors
+            status = WebServerStatus::web_server_close;
+            break;
+        }
+
+        if (client.available()) {             // if there's bytes to read from the client,
+            char c = client.read();             // read a byte, then
+            if (c == '\n') {                    // if the byte is a newline character
+
+                // if the current line is blank, you got two newline characters in a row.
+                // that's the end of the client HTTP request, so send a response:
+                if (currentLine->length() == 0) {
+                    // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                    // and a content-type so the client knows what's coming, then a blank line:
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-type:text/html");
+                    client.println();
+
+                    client.print("<h1>Arduino Doorbell</h1>");
 //                        client.print("<h3>Battery Voltage</h3>");
 //                        client.print("<dl>");
 //                        client.print("<dt>ADC Output</dt>");
@@ -46,34 +62,40 @@ void WebServer::loop() {
 //                        client.print(get_battery_voltage());
 //                        client.print("</dd>");
 
-                        #ifdef ARDUINO_ARCH_SAMD
-                        client.print("<dl>");
-                        client.print("<dt>Firmware Version</dt>");
-                        client.print("<dd>");
-                        client.print(AUTO_VERSION);
-                        client.print("<dt>WiFiNINA Firmware Version</dt>");
-                        client.print("<dd>");
-                        client.print(WiFi.firmwareVersion());
-                        client.print("</dd>");
-                        #endif
+                    #ifdef ARDUINO_ARCH_SAMD
+                    client.print("<dl>");
+                    client.print("<dt>Firmware Version</dt>");
+                    client.print("<dd>");
+                    client.print(AUTO_VERSION);
+                    client.print("<dt>WiFiNINA Firmware Version</dt>");
+                    client.print("<dd>");
+                    client.print(WiFi.firmwareVersion());
+                    client.print("</dd>");
+                    #endif
 
-                        if (rootPageHandler != NULL) {
-                            rootPageHandler(&client);
-                        }
-
-                        // The HTTP response ends with another blank line:
-                        client.println();
-                        // break out of the while loop:
-                        break;
-                    } else {    // if you got a newline, then clear currentLine:
-                        currentLine = "";
+                    if (rootPageHandler != NULL) {
+                        rootPageHandler(&client);
                     }
-                } else if (c != '\r') {  // if you got anything else but a carriage return character,
-                    currentLine += c;      // add it to the end of the currentLine
+
+                    // The HTTP response ends with another blank line:
+                    client.println();
+                    status = WebServerStatus::web_server_close;
+                } else {    // if you got a newline, then clear currentLine:
+                    currentLine->operator=("");
                 }
+            } else if (c != '\r') {  // if you got anything else but a carriage return character,
+                currentLine->concat(c);      // add it to the end of the currentLine
             }
         }
-        // close the connection:
-        client.stop();
-    }
+        break;
+
+    case WebServerStatus::web_server_close:
+        if (client.connected()) {
+            client.stop();
+        }
+        free(currentLine);
+        status = WebServerStatus::web_server_idle;
+        break;
+
+    } // switch
 }
